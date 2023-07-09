234246f28ed8c3083d832c17da2b30a892070eb5
kane <lisaxkane@gmail.com>
2023-07-08 23:34:57 -0500

    yea

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 const SentimentData = async (url) => {
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   page.setDefaultNavigationTimeout(0);
   await page.goto(url);
